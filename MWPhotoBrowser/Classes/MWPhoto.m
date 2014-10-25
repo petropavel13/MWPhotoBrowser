db1ef65efe06b881824b521e306ca6b1251fa22d
petropavel13 <mail4work0xFF@gmail.com>
2014-10-25 22:05:57 +0600

    retry failed

--- MWPhotoBrowser/Classes/MWPhoto.m
+++ MWPhotoBrowser/Classes/MWPhoto.m
@@ -156,7 +156,7 @@
             @try {
                 SDWebImageManager *manager = [SDWebImageManager sharedManager];
                 _webImageOperation = [manager downloadImageWithURL:_photoURL
-                                                           options:0
+                                                           options:SDWebImageRetryFailed
                                                           progress:^(NSInteger receivedSize, NSInteger expectedSize) {
                                                               if (expectedSize > 0) {
                                                                   float progress = receivedSize / (float)expectedSize;
